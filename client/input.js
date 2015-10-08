9106eadeb2cdd376cd2f91d172da2695266cd363
Jr Reyes <jr.evilboss@gmail.com>
2015-10-08 10:10:53 +0800

    Starter slack template

---
+++ client/input.js
@@ -0,0 +1,14 @@
+Template.footer.events({
+  'keypress input': function(e) {
+    var inputVal = $('.input-box_text').val();
+    if(!!inputVal) {
+      var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
+      if (charCode == 13) {
+        e.stopPropagation();
+        Meteor.call('newMessage', {text: $('.input-box_text').val()});
+        $('.input-box_text').val("");
+        return false;
+      }    
+    }
+  }
+});
